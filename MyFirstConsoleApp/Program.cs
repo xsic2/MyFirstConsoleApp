0f30c8c5b0a526e0c4edf266dc64e46ed02edc9f
Natalya <Natalya@DESKTOP-A3C9OUH>
2023-09-04 14:53:57 +0400

    Добавьте файлы проекта.

---
+++ MyFirstConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// ура мой первый код
+namespace Sahsapisya
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            system.console.writeline("Саша пися!");
+        }
+        }
+    }
